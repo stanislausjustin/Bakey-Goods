c72e0b51a4ecb2785d65cef6fafd3c6d5415a9e2
Jaztin <144786378+stanislausjustin@users.noreply.github.com>
2023-12-15 17:12:30 +0700

    Add files via upload

---
+++ After sign in/app.js
@@ -0,0 +1,65 @@
+//IMAGE SLIDER
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll ('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function(){
+    if(active + 1 > lengthItems){
+        active = 0;
+    }else{
+        active = active + 1;
+    }
+    reloadSlider();
+}
+prev.onclick = function(){
+    if(active - 1 < 0){
+        active = lengthItems;
+    }else{
+        active = active -1;
+    }
+    reloadSlider();
+}
+let refreshSlider = setInterval((()=> {next.click()}), 3000);
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => { next.click() }, 3000); 
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function(){
+        active = key;
+        reloadSlider();
+    })
+})
+
+// NAVBAR
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
+
+
+
+
