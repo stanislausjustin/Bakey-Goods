a541286d47736b57203739ac442dc847323c301f
Agri <98445091+agrianshah@users.noreply.github.com>
2023-12-17 14:07:09 +0700

    Update contactus.js

--- Contact Us/contactus.js
+++ Contact Us/contactus.js
@@ -1,25 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM content loaded");
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
 
-  let navToggle = document.querySelector(".toggle-btn");
-  let nav_first_ul = document.querySelector(".nav.navbar-nav");
-  let nav_second_ul = document.querySelector(".nav.navbar-nav.navbar-right");
+navToggle.addEventListener("click", function () {
+    console.log("JavaScript is working!");
 
-  navToggle.addEventListener("click", function () {
-    console.log("Toggle button clicked");
-
-    if (nav_first_ul.classList.contains("active")) {
-      console.log("Closing menu");
-      this.setAttribute("aria-expanded", "false");
-      this.setAttribute("aria-label", "menu");
-      nav_first_ul.classList.remove("active");
-      nav_second_ul.classList.remove("keep-right");
-    } else {
-      console.log("Opening menu");
-      nav_first_ul.classList.add("active");
-      nav_second_ul.classList.add("keep-right");
-      this.setAttribute("aria-label", "close menu");
-      this.setAttribute("aria-expanded", "true");
-    }
-  });
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
 });
